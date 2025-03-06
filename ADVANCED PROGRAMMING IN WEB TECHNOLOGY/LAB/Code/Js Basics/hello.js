13c89cef0faf4335c8100fd8bdf7f8683793a0ac
Towsif <towsif1528@gmail.com>
2025-03-06 22:18:12 +0600

    js code practice

---
+++ ADVANCED PROGRAMMING IN WEB TECHNOLOGY/LAB/Code/Js Basics/hello.js
@@ -0,0 +1,23 @@
+// console.log("Hello world");
+var a = 10;
+// console.log(typeof a);
+// for (let i = 0; i < 10; i++) {
+//   console.log(i * a);
+// }
+
+let b = 10;
+console.log(b);
+{
+  let b = 20;
+  console.log(20);
+}
+
+const c = [1, 2, 3, 4, 5];
+console.log(typeof c);
+c.forEach((element) => {
+  console.log(element);
+});
+
+c.forEach((element) => {
+  console.log(element);
+});
