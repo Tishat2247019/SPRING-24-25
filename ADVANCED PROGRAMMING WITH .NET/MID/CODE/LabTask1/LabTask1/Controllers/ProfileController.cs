8569a262a30bde25bfc52de1b6c874626adbb902
Towsif <towsif1528@gmail.com>
2025-03-17 19:27:04 +0600

    added enginerring management lecture slide

---
+++ ADVANCED PROGRAMMING WITH .NET/MID/CODE/LabTask1/LabTask1/Controllers/ProfileController.cs
@@ -0,0 +1,56 @@
+﻿using LabTask1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabTask1.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+            Education e1 = new Education()
+            {
+                degree = "MSC",
+                result = "4.00",
+                year = 2026
+            };
+            Education e2 = new Education()
+            {
+                degree = "BSC",
+                result = "3.99",
+                year = 2025
+        };
+        Education e3 = new Education()
+        {
+            degree = "HSC",
+            result = "5.00",
+            year = 2020
+        };
+            Education[] educations = new Education[] { e1, e2, e3 };
+            return View(educations);
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+        public ActionResult Ref()
+        {
+            return View();
+        }
+    }
+}
---
+++ ADVANCED PROGRAMMING WITH .NET/MID/CODE/LabTask1/LabTask1/Models/Education.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LabTask1.Models
+{
+    public class Education
+    {
+        public string degree { get; set; }
+        public string result { get; set; }
+        public int year { get; set; }
+    }
+}
