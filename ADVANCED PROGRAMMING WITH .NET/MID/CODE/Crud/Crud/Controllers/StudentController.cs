144c9e7178ca141fb8b96f7868cb7c1001dfdcd0
Towsif <towsif1528@gmail.com>
2025-04-12 22:27:56 +0600

    adv dot net

---
+++ ADVANCED PROGRAMMING WITH .NET/MID/CODE/Crud/Crud/Controllers/StudentController.cs
@@ -0,0 +1,55 @@
+﻿using Crud.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Crud.Controllers
+{
+    public class StudentController : Controller
+    {
+        practiceEntities db = new practiceEntities();
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            //for listing all students
+
+            var data = db.Students.ToList();
+            return View(data);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+            //no validation
+            db.Students.Add(s);
+            //db.SaveChanges();//return the number of rows affected
+
+            if (db.SaveChanges() > 0)
+            {
+                TempData["Msg"] = "Student added";
+                return RedirectToAction("Index");
+            }
+            TempData["msg"] = "Not Succesfull";
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var student = db.Students.Find(id);
+
+            return View(student);
+
+        }
+
+    }  
+}
---
+++ ADVANCED PROGRAMMING WITH .NET/MID/CODE/CrudPractice/CrudPractice/Controllers/StudentController.cs
@@ -0,0 +1,45 @@
+﻿using CrudPractice.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CrudPractice.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            var db = new crudEntities();
+            var data = db.Students.ToList();
+            return View(data);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+            var db = new crudEntities();
+            db.Students.Add(s);
+            
+            if(db.SaveChanges() > 0)
+            {
+               return RedirectToAction("Index");
+            }
+            return View(s);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var db = new crudEntities();
+            var student_data = db.Students.Find(id);
+            return View(student_data);
+        }
+    }
+}
